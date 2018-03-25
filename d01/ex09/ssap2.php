b1e7adf87349912a75619e60e9bdc30e0327a0bf
Frederic BERTOIA <frederic.bertoia@student.42.fr>
2018-03-25 23:44:21 +0200

    last

---
+++ d01/ex09/ssap2.php
@@ -0,0 +1,37 @@
+#!/usr/bin/php
+<?PHP
+function sort_ex($str1, $str2)
+{
+	$str_a = $str1;
+	$str_b = $str2;
+	$str_a = strtolower($str_a);
+	$str_b = strtolower($str_b);
+	$base = "abcdefghijklmnopqrstuvwxyz0123456789";
+	for ($i = 0; $str_a[$i] && $str_b[$i] ; $i++)
+	{
+		if ($str_a[$i] != $str_b[$i])
+		{
+			if (($ret1 = strpos($base, $str_a[$i])) === FALSE)
+				(int)$ret1 = ord((int)$str_a[$i]) + 40;
+			if (($ret2 = strpos($base, $str_b[$i])) === FALSE)
+				(int)$ret2 = ord($str_b[$i]) + 40;
+			return ($ret1 - $ret2);
+		}
+	}
+	return ($str_a[$i] - $str_b[$i]);
+
+}
+
+if ($argc == 1)
+	return (0);
+$i = 1;
+while ($i < count($argv))
+{
+	$arr = preg_split('/ +/', $argv[$i]);
+	$ret = array_merge((array)$ret, $arr);
+	$i++;
+}
+usort($ret, 'sort_ex');
+for ($i = 0; $i < count($ret) ; $i++)
+		printf("$ret[$i]\n");
+?>
